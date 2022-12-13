9705635bafa35e32fa2af73fe5fd3842ef0a28c6
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-13 14:27:41 -0300

    Agregue bootstrap al proyecto. Genere un nav, un footer y un header

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
