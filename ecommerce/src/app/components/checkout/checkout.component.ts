bc9a78fc79b44d2dfbb786619a835559ff56b8e5
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-16 19:14:45 -0300

    Termine la estructura del formulario. Falta la logica para enviar los datos del carrito

--- ecommerce/src/app/components/checkout/checkout.component.ts
+++ ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+
+  public firstName : string | undefined;
+  public lastName : string | undefined;
+  public email : string | undefined;
+  public address : string | undefined;
+  public numero: string | undefined;
+  public piso: string | undefined;
+  public depto: string | undefined;
+  public debito : string | undefined;
+  public credito : string | undefined;
+  public nombreTarjeta : string | undefined;
+  public numeroTarjeta : string | undefined;
+  public expiracionTarjeta : string | undefined;
+  public cvvTarjeta : string | undefined;
+
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  public submitForm(){
+    alert("Formulario enviado");
+  }
 
 }
