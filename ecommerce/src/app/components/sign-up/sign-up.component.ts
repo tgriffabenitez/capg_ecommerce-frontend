8a9ab040d101e69db4e6da46517bf2e62d56b563
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2022-12-17 01:55:12 -0300

    cree el componente para registrarse a la pagina

---
+++ ecommerce/src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+
+  constructor() { }
+
+  public inputEmail : string = "";
+  public inputPassword : string = "";
+
+  onSumbitForm(data : any){
+
+
+  }
+
+}
